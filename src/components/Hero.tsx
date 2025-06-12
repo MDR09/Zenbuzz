cc390b492720b8117bc234cc153d6dd379a8a583
manish <manishjini29@gmail.com>
2025-06-12 16:10:04 +0530

    first commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -44,7 +44,7 @@ const Hero: React.FC = () => {
       {/* Background Video */}
       <video
         ref={videoRef}
-        src="/data/background.mp4"
+        src="/assets/background.mp4"
         className="absolute inset-0 w-full h-full object-cover"
         autoPlay
         muted
